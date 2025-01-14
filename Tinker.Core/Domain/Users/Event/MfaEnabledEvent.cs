a3119e4d6619ef5b69d996772001134a8ae74d3a
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-01-14 10:44:49 +0100

    feat: add MfaEnabledEvent and MfaDisabledEvent for multi-factor authentication state changes

---
+++ Tinker.Core/Domain/Users/Event/MfaEnabledEvent.cs
@@ -0,0 +1,11 @@
+using Tinker.Core.Domain.Common.Interfaces;
+using Tinker.Core.Domain.Users.ValueObjects;
+
+namespace Tinker.Core.Domain.Users.Event;
+
+public record MfaEnabledEvent(UserId UserId) : IDomainEvent
+{
+    public DateTime OccurredOn { get; } = DateTime.UtcNow;
+
+    public Guid EventId { get; } = Guid.NewGuid();
+}
